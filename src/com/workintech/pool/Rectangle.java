3934340080fa4747208d4812d70e39f852dad345
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-19 21:19:19 +0300

    constructor olusturuldu

--- src/com/workintech/pool/Rectangle.java
+++ src/com/workintech/pool/Rectangle.java
@@ -3,4 +3,9 @@ package com.workintech.pool;
 public class Rectangle {
     private int width;
     private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = (width < 0) ? 0 : width;
+        this.height = (height < 0) ? 0 : height;
+    }
 }
