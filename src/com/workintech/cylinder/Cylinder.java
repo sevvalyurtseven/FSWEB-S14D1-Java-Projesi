25573079d384fa279e746375508d1ae654075dc2
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-19 21:02:09 +0300

    instance variable eklendi, constructor guncellendi

--- src/com/workintech/cylinder/Cylinder.java
+++ src/com/workintech/cylinder/Cylinder.java
@@ -1,7 +1,14 @@
 package com.workintech.cylinder;
 
 public class Cylinder extends Circle{
-    public Cylinder(double radius) {
+
+    private double height;
+    public Cylinder(double radius, double height) {
         super(radius);
+        if(height < 0){
+            this.height = 0;
+        }else{
+            this.height = height;
+        }
     }
 }
