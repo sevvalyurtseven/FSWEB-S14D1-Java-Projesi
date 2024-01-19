42c883c4992a76ee0933584ff638f08266bbf0ab
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-19 22:55:18 +0300

    1 adet constructor ve her degisken icin getter setter methodlari olusturuldu

--- src/com/workintech/developer/Employee.java
+++ src/com/workintech/developer/Employee.java
@@ -4,4 +4,34 @@ public class Employee {
     private long id;
     private String name;
     private double salary;
+
+    public Employee(long id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
