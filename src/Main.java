9bd13e0383be90ebbfd9b80fe6e85e2f50ad2838
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-19 21:32:08 +0300

    havuzun alanini hesaplama gorevi tamamlandi ve methodlar test edildi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import com.workintech.cylinder.Circle;
 import com.workintech.cylinder.Cylinder;
+import com.workintech.pool.Cuboid;
+import com.workintech.pool.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,5 +18,21 @@ public class Main {
         System.out.println("cylinder.height= " + cylinder.getHeight());
         System.out.println("cylinder.area= " + cylinder.getArea());
         System.out.println("cylinder.volume= " + cylinder.getVolume());
+
+        //HAVUZUN ALANI ICIN:
+
+        Rectangle rectangle = new Rectangle(5, 10);
+
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+
+        Cuboid cuboid = new Cuboid(5,10,5);
+
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
     }
 }
